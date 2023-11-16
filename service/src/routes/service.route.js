e5f88924f10dad28f41d51070cf7125a724ff0d8
GauravKumarmishra <gaurav.kumar@srijan.net>
2023-11-16 13:31:38 +0530

    update service file

--- service/src/routes/service.route.js
+++ service/src/routes/service.route.js
@@ -5,8 +5,7 @@ const serviceRouter = Router();
 
 serviceRouter.post('/', async (req, res) => {
   logger.info('Cart update extension executed' , req.body);
-  res.status(200);
-  res.send();
+  res.status(200).json({"message": "success"});
 });
 
 export default serviceRouter;
